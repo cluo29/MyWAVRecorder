f5ad09fd27f950e16572fe8c4dc487583b286154
Chu Luo <mr.cluo29@gmail.com>
2016-09-13 13:46:08 +0200

    Initial commit

---
+++ app/src/main/java/io/cluo29/github/mywavrecorder/MainActivity.java
@@ -0,0 +1,44 @@
+package io.cluo29.github.mywavrecorder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ExtAudioRecorder extAudioRecorder;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        extAudioRecorder = ExtAudioRecorder.getInstanse(false);
+
+        ;
+
+
+        findViewById(R.id.button).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                String fileName = System.currentTimeMillis() + ".wav";
+                extAudioRecorder.setOutputFile(getApplicationContext().getExternalFilesDir(null)+ fileName);
+                extAudioRecorder.prepare();
+                extAudioRecorder.start();
+            }
+        });
+
+        findViewById(R.id.button2).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                extAudioRecorder.stop();
+                extAudioRecorder.release();
+            }
+        });
+    }
+
+
+
+}
